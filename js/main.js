86b52831198a65cd3ec56240ffdbee8eccdd504f
Douglas Moro <douglas.moro@unochapeco.edu.br>
2021-07-29 23:08:50 -0300

    Aula 12 consluída

--- js/main.js
+++ js/main.js
@@ -10,6 +10,9 @@ const start = () => {
     $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
     //PRINCIPAIS VARIÁVEIS DO JOGO
+    var podeAtirar = true;
+    var velocidade = 5;
+    var posicaoY = parseInt(Math.random() * 334);
     let jogo = {};
     let TECLA = {
         W: 87,
@@ -31,6 +34,9 @@ const start = () => {
     function loop() {
         movefundo();
         movejogador();
+        moveinimigo1();
+        moveinimigo2();
+        moveamigo();
     }
 
     function movefundo() {
@@ -42,17 +48,80 @@ const start = () => {
         if (jogo.pressionou[TECLA.W]) {
             var topo = parseInt($("#jogador").css("top"));
             $("#jogador").css("top", topo - 10);
+            if (topo <= 0) {
+                $("#jogador").css("top", topo + 0);
+            }
         }
 
         if (jogo.pressionou[TECLA.S]) {
             var topo = parseInt($("#jogador").css("top"));
             $("#jogador").css("top", topo + 10);
+            if (topo >= 434) {
+                $("#jogador").css("top", topo - 0);
+            }
         }
 
         if (jogo.pressionou[TECLA.D]) {
-            //Chama função Disparo	
+            disparo();
         }
     }
+
+    function moveinimigo1() {
+        posicaoX = parseInt($("#inimigo1").css("left"));
+        $("#inimigo1").css("left", posicaoX - velocidade);
+        $("#inimigo1").css("top", posicaoY);
+
+        if (posicaoX <= 0) {
+            posicaoY = parseInt(Math.random() * 334);
+            $("#inimigo1").css("left", 694);
+            $("#inimigo1").css("top", posicaoY);
+        }
+    }
+
+    function moveinimigo2() {
+        posicaoX = parseInt($("#inimigo2").css("left"));
+        $("#inimigo2").css("left", posicaoX - 3);
+        if (posicaoX <= 0) {
+            $("#inimigo2").css("left", 775);
+        }
+    }
+
+    function moveamigo() {
+        posicaoX = parseInt($("#amigo").css("left"));
+        $("#amigo").css("left", posicaoX + 1);
+        if (posicaoX > 906) {
+            $("#amigo").css("left", 0);
+        }
+    }
+
+    function disparo() {
+        if (podeAtirar == true) {
+            podeAtirar = false;
+            topo = parseInt($("#jogador").css("top"))
+            posicaoX = parseInt($("#jogador").css("left"))
+            tiroX = posicaoX + 190;
+            topoTiro = topo + 37;
+            $("#fundoGame").append("<div id='disparo'></div");
+            $("#disparo").css("top", topoTiro);
+            $("#disparo").css("left", tiroX);
+
+            var tempoDisparo = window.setInterval(executaDisparo, 30);
+        }
+
+        function executaDisparo() {
+            posicaoX = parseInt($("#disparo").css("left"));
+            $("#disparo").css("left", posicaoX + 15);
+
+            if (posicaoX > 900) {
+                window.clearInterval(tempoDisparo);
+                tempoDisparo = null;
+                $("#disparo").remove();
+                podeAtirar = true;
+            }
+        } // Fecha executaDisparo()
+    }
 }
+
 btnStart.addEventListener('click', start);
+
 
